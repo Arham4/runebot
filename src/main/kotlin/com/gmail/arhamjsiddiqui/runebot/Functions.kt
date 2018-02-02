9bef3bcbf774ea4b1cd28a9398a255236f599da3
Arham4 <arhamjsiddiqui@gmail.com>
2018-02-01 22:50:07 -0600

    optimizations to player creation

--- src/main/kotlin/com/gmail/arhamjsiddiqui/runebot/Functions.kt
+++ src/main/kotlin/com/gmail/arhamjsiddiqui/runebot/Functions.kt
@@ -1,10 +1,7 @@
 package com.gmail.arhamjsiddiqui.runebot
 
-import com.gmail.arhamjsiddiqui.runebot.jooq.tables.Players
 import com.gmail.arhamjsiddiqui.runebot.player.Player
 import net.dv8tion.jda.core.entities.User
-import org.jooq.SQLDialect
-import org.jooq.impl.DSL
 
 /**
  * A random assortment of functions (include extension functions here too)
@@ -13,34 +10,9 @@ import org.jooq.impl.DSL
  */
 object DatabaseFunctions {
     fun fetchPlayer(user: User): Player {
-        var player = RuneBot.players[user]
-        return player ?: let {
-            player = fetchPlayerByDatabase(user)
-            player ?: makePlayer(user)
-        }
-    }
-
-    /**
-     * Need to make this method more efficient
-     */
-    private fun fetchPlayerByDatabase(user: User): Player? {
-        val player = Player(user)
-        if (player.exists) RuneBot.players.put(user, player)
-        return if (player.exists) player else null
-    }
-
-    private fun makePlayer(user: User): Player {
-        DSL.using(RuneBot.DATASOURCE, SQLDialect.POSTGRES).insertInto(Players.PLAYERS)
-                .set(Players.PLAYERS.DISCORD_ID, user.id)
-                .set(Players.PLAYERS.TOTAL_LEVEL, 0)
-                .set(Players.PLAYERS.TOTAL_EXP, 0)
-                .set(Players.PLAYERS.LEVELS, Array(25, {0}))
-                .set(Players.PLAYERS.EXPERIENCES, Array(25, {0}))
-                .execute()
-        val player = Player(user)
-        RuneBot.players.put(user, player)
-        RuneBot.BOT.sendMessage("Welcome to RuneBot ${user.asMention}! Your account has successfully been created!")
-        return player
+        return RuneBot.players[user] ?: Player(user)
     }
 }
+
+
 
--- src/main/kotlin/com/gmail/arhamjsiddiqui/runebot/player/Player.kt
+++ src/main/kotlin/com/gmail/arhamjsiddiqui/runebot/player/Player.kt
@@ -3,6 +3,7 @@ package com.gmail.arhamjsiddiqui.runebot.player
 import com.gmail.arhamjsiddiqui.runebot.RuneBot
 import com.gmail.arhamjsiddiqui.runebot.jooq.tables.Players
 import com.gmail.arhamjsiddiqui.runebot.jooq.tables.records.PlayersRecord
+import com.gmail.arhamjsiddiqui.runebot.sendMessage
 import net.dv8tion.jda.core.entities.User
 import org.jooq.SQLDialect
 import org.jooq.SelectConditionStep
@@ -15,7 +16,6 @@ import org.jooq.impl.DSL
  */
 class Player(private val user: User) {
     val skills: Skills = Skills(this)
-    var exists = false
 
     /**
      * Allows us to use the player directly rather than always looking them up with their discord ID using DSL
@@ -29,11 +29,29 @@ class Player(private val user: User) {
     init {
         val playerSQL = sql?.fetchAny()
         if (playerSQL != null) {
-            skills.totalLevel = playerSQL.totalLevel
-            skills.totalExp = playerSQL.totalExp
-            skills.levels = playerSQL.levels
-            skills.experiences = playerSQL.experiences
-            exists = true
+            instantiateVariables(playerSQL)
+        } else {
+            makePlayer()
         }
+    }
+
+    private fun instantiateVariables(playerSQL: PlayersRecord) {
+        skills.totalLevel = playerSQL.totalLevel
+        skills.totalExp = playerSQL.totalExp
+        skills.levels = playerSQL.levels
+        skills.experiences = playerSQL.experiences
+    }
+
+    private fun makePlayer() {
+        DSL.using(RuneBot.DATASOURCE, SQLDialect.POSTGRES).insertInto(Players.PLAYERS)
+                .set(Players.PLAYERS.DISCORD_ID, user.id)
+                .set(Players.PLAYERS.TOTAL_LEVEL, 0)
+                .set(Players.PLAYERS.TOTAL_EXP, 0)
+                .set(Players.PLAYERS.LEVELS, Array(25, {0}))
+                .set(Players.PLAYERS.EXPERIENCES, Array(25, {0}))
+                .execute()
+        instantiateVariables(sql!!.fetchAny()) // !! because we just added them above, so there is no way it would be null.
+        RuneBot.players.put(user, this)
+        RuneBot.BOT.sendMessage("Welcome to RuneBot ${user.asMention}! Your account has successfully been created!")
     }
 }
