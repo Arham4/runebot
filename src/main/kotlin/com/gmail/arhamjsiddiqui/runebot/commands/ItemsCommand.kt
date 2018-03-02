e80f550d93ea9eb5d0adec27ea589ca67ea80e22
Arham4 <arhamjsiddiqui@gmail.com>
2018-03-01 21:26:09 -0600

    corrected item command logic

--- src/main/kotlin/com/gmail/arhamjsiddiqui/runebot/commands/ItemsCommand.kt
+++ src/main/kotlin/com/gmail/arhamjsiddiqui/runebot/commands/ItemsCommand.kt
@@ -1,6 +1,7 @@
 package com.gmail.arhamjsiddiqui.runebot.commands
 
 import com.gmail.arhamjsiddiqui.runebot.CommandFunctions
+import com.gmail.arhamjsiddiqui.runebot.DiscordFunctions.queueMessage
 import de.btobastian.sdcf4j.Command
 import de.btobastian.sdcf4j.CommandExecutor
 import net.dv8tion.jda.core.entities.TextChannel
@@ -9,14 +10,15 @@ import net.dv8tion.jda.core.entities.User
 class ItemsCommand : CommandExecutor {
 
     @Command(aliases = ["r.items"], description = "Shows the items you have.")
-    fun onItemsCommand(user: User, textChannel: TextChannel): String {
-        var message = "Items in inventory:\n```\n"
+    fun onItemsCommand(user: User, textChannel: TextChannel) {
         CommandFunctions.withPlayer(user, textChannel) { player ->
+            var message = "Items in inventory:\n```\n"
+
             player.items.forEach { item ->
                 message += "${item.count}x ${item.name}\n"
             }
+            message += "```"
+            textChannel.queueMessage(message)
         }
-        message += "```"
-        return message
     }
 }
