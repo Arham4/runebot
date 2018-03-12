2f3c76f5cc1685e503a1788a1c353baece9dc099
Arham4 <arhamjsiddiqui@gmail.com>
2018-03-11 22:28:38 -0500

    new exp formula

--- src/main/kotlin/com/gmail/arhamjsiddiqui/runebot/commands/TrainCommand.kt
+++ src/main/kotlin/com/gmail/arhamjsiddiqui/runebot/commands/TrainCommand.kt
@@ -22,7 +22,6 @@ class TrainCommand : CommandExecutor {
             textChannel.queueMessage("Incorrect usage! Use as: `r.train SKILL`")
             return
         }
-        val exp = ThreadLocalRandom.current().nextInt(1, 50)
         withPlayer(user, textChannel) { player ->
             val skillName = let {
                 val input = args[0].toLowerCase()
@@ -30,6 +29,7 @@ class TrainCommand : CommandExecutor {
             }
             val skillId = SkillsData.skills.skillIdFor[skillName]
             if (skillId != null) {
+                val exp = ThreadLocalRandom.current().nextInt(15 * player.skills.levels[skillId], if (ThreadLocalRandom.current().nextInt(0, 100) == 1) Math.pow(15 * player.skills.levels[skillId].toDouble(), 1.5).toInt() else Math.pow(15 * player.skills.levels[skillId].toDouble(), 1.2).toInt())
                 player.train(skillId, exp)
                 textChannel.queueMessage("You have earned $exp EXP! Total EXP in ${skillName.capitalize()}: "
                         + player.skills.experiences[skillId])
